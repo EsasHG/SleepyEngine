bbd058c0c3edc75a90ffcaef20b5772a5c562e5c
Espen <espenhg@hotmail.com>
2022-09-02 18:08:25 +0200

    Basic quad now rendering!! :D

--- SleepyEngine/Application.cpp
+++ SleepyEngine/Application.cpp
@@ -1,13 +1,19 @@
 #include "Application.h"
 #include <iostream>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/norm.hpp>
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 
 int Application::Run()
 {
-	int windowWidth = 1000, windowHeight = 1000;
-	
+	int windowWidth = 800, windowHeight = 600;
 	
 	if (!glfwInit())
 		return -1;
@@ -37,13 +43,135 @@ int Application::Run()
 		std::cout << "Glad did not load successfully!" << std::endl;
 	}
 
-	while (!glfwWindowShouldClose(window))
+	float quadVertices[] = {
+		// positions   // texCoords
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f
+	};
+
+
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(4 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	std::ifstream v("Shaders/SingleColor.vert");
+	std::string vertString;
+	if (v)
+	{
+		std::ostringstream ss;
+		ss << v.rdbuf();
+		vertString = ss.str();
+	}
+	
+	std::ifstream f("Shaders/SingleColor.frag");
+
+	std::string fragString;
+	if (f)
+	{
+		std::ostringstream ss;
+		ss << f.rdbuf();
+		fragString = ss.str();
+	}
+
+	const char* vert = vertString.c_str();
+	const char* frag = fragString.c_str();
+	int success;
+
+
+	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertShader, 1, &vert, NULL);
+	glCompileShader(vertShader);
+	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &success);
+	if (!success)
 	{
 
+		int logLength;
+		glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+		char* infoLog = new char[logLength];
+		glGetShaderInfoLog(vertShader, logLength, NULL, infoLog);
+		std::cout << "VERTEX SHADER COMPILATION FAILED:" << infoLog << std::endl;
+		delete[] infoLog;
+		glDeleteShader(vertShader);
+	}
+
+	
+	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragShader, 1, &frag, NULL);
+	glCompileShader(fragShader);
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		int logLength;
+		glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+		char* infoLog = new char[logLength];
+		glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+		std::cout << "FRAGMENT SHADER COMPILATION FAILED:" << infoLog << std::endl;
+		delete[] infoLog;
+		glDeleteShader(fragShader);
+	}
+
+	unsigned int Id = glCreateProgram();
+	glAttachShader(Id, vertShader);
+	glAttachShader(Id, fragShader);
+	glLinkProgram(Id);
+
+	int isLinked;
+	glGetProgramiv(Id, GL_LINK_STATUS, &isLinked);
+	if (!isLinked)
+	{
+		int logLength;
+		glGetProgramiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+		char* infoLog = new char[logLength];
+		glGetProgramInfoLog(fragShader, 512, NULL, infoLog);
+		std::cout << "SHADER LINKING FAILED:" << infoLog << std::endl;
+		delete[] infoLog;
+		glDeleteProgram(fragShader);
+		glDeleteShader(vertShader);
+		glDeleteShader(fragShader);
+
+	}
+	glDetachShader(Id, vertShader);
+	glDetachShader(Id, fragShader);
+	glUseProgram(Id);
+
+	glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 4.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	int loc = glGetUniformLocation(Id, "view");
+	glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(view));
+
+	glm::mat4 projection = glm::perspective(0.5f, (float)windowWidth / windowHeight, 0.1f, 10.0f);
+	loc = glGetUniformLocation(Id, "projection");
+	glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	while (!glfwWindowShouldClose(window))
+	{
 		glClearColor(0.7f, 0.3f, 0.6f,1.0f);
 		glViewport(0, 0, windowWidth, windowHeight);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
+		glUseProgram(Id);
+		loc = glGetUniformLocation(Id, "color");
+		glUniform3f(loc, glm::sin(glfwGetTime()-1) / 2 + 0.5, glm::sin(glfwGetTime())/2 + 0.5, glm::sin(glfwGetTime()+1) / 2 + 0.5);
+
+		glm::mat4 model = glm::mat4(1.0f);
+		loc = glGetUniformLocation(Id, "model");
+		glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(model));
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
